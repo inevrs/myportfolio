0a8e1ed78818cdd3dbe690d7bf15a2a88d2b8216
inevrs <isyrafnas2009@gmail.com>
2026-06-11 17:55:18 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portfolio",
+  basePath: "/myportfolio",
   images: {
     unoptimized: true,
   },
 };
-
 export default nextConfig;
